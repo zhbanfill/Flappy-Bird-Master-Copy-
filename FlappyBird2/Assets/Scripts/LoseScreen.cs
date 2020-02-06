eba729f5735aacf3875ec14cca41cf1deb99047d
nihil-novi-sub-sole <eukariah.hunter@digipen.edu>
2020-02-06 14:37:25 -0800

    New version use FlappyBird2/.gitignore

---
+++ FlappyBird2/Assets/Scripts/LoseScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseScreen : MonoBehaviour
+{
+    public Sprite noMedal;
+    public Sprite bronzeMedal;
+    public Sprite silverMedal;
+    public Sprite goldMedal;
+
+    public Image medal;
+    public Text highscoreText;
+
+    public GameObject loseWindow;
+    
+	void Update ()
+    {
+        loseWindow.SetActive(BirdController.gameOver);
+
+        //Assign badges and show the highscore at the end of the game.
+        if(loseWindow.activeSelf)
+        {
+            highscoreText.text = BirdController.highscoreDisplay;
+
+            if (BirdController.score < 5)
+                medal.sprite = noMedal;
+            else if (BirdController.score < 10)
+                medal.sprite = bronzeMedal;
+            else if (BirdController.score <= 20)
+                medal.sprite = silverMedal;
+            else
+                medal.sprite = goldMedal;
+        }
+
+        if(BirdController.score > BirdController.highestScore)
+        {
+            BirdController.highestScore = BirdController.score;
+        }
+	}
+
+    public void ResetGame()
+    {
+        //Reload the scene.
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
---
+++ FlappyBird2/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [Tooltip("`Text` is the text object we will be displaying score to.")]
+    public Text text;
+
+	void Update ()
+    {
+        //Updates the text objects text field with the score we currently have.
+        text.text = BirdController.scoreDisplay;
+	}
+}
